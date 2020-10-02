21c63d225d62c8c56200707817cbdc9423f1cfef
Aubrey Ivan Apungan <apungan.aubreyivansunga@gmail.com>
2020-10-02 22:34:56 +0800

    initial frontend setup

---
+++ ares-front/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>This is the initial Application Setup</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
