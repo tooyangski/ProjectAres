7bb444638489fbc625b18d9d0cc982e40f107d14
Aubrey Ivan Apungan <apungan.aubreyivansunga@gmail.com>
2020-10-03 00:05:42 +0800

    Added bulma css

--- ares-front/src/App.js
+++ ares-front/src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
-import "./App.css";
+import React, { Suspense } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>This is the initial Application Setup</p>
-      </header>
+    <div className="container">
+      <div className="notification is-primary">
+        This container is <strong>centered</strong> on desktop and larger
+        viewports.
+      </div>
     </div>
   );
-}
+};
 
 export default App;
